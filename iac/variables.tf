a12c0f89786115b6620ce44b3100d977200670fe
Daniela <144456230+Dxnnie17@users.noreply.github.com>
2026-04-17 01:35:58 +0000

    agregado el dev en web, api y db

--- iac/variables.tf
+++ iac/variables.tf
@@ -9,3 +9,15 @@ variable "api_port" {
 variable "db_port" {
   default = 4003
 }
+
+variable "web_dev_port" {
+  default = 5001
+}
+
+variable "api_dev_port" {
+  default = 5002
+}
+
+variable "db_dev_port" {
+  default = 5003
+}
